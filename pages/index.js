62a82bac03981a7aa1511a43bbd891689a205ea6
Zielihey <cleabernadette.payra@gmail.com>
2021-04-05 02:17:20 -0700

    'login/request'

--- pages/index.js
+++ pages/index.js
@@ -55,8 +55,10 @@ export default function Home() {
   // punta kayo sa localhost:3000/login pagtapos nito
 
   return (
-    <div>
-      <h1>Request Form</h1>
+    <div class="bg-gradient-to-r from-pink-300 to-yellow-200 py-32 px-10 min-h-screen">
+      <h3 class="text-5xl font-normal leading-normal mt-0 mb-2 text-pink-800 text-center">
+          Request Form
+      </h3>
 
       {response ? (
         <div className="flex">
@@ -71,52 +73,54 @@ export default function Home() {
         </div>
       ) : null}
 
-      <form onSubmit={submitHandler}>
-        <div>
-          <label>Name:</label>
-          <input type="text" name="name" onChange={inputHandler} />
+      <form class="md:w-1/2 mx-auto" onSubmit={submitHandler}>
+        <div class="shadow-xl">
+          <div class="flex items-center bg-purple-400 rounded-t-lg border-b border-purple-500">
+          <label for="name" class="w-16 text-right font-semibold mr-16">Name:</label>
+          <input type="text" id="name" name="name" class="flex-1 p-4 pl-0 bg-transparent outline-none text-white ml-16" onChange={inputHandler} />
         </div>
-        <div>
-          <label>Email:</label>
-          <input type="text" name="email" onChange={inputHandler} />
+        <div class="flex items-center bg-purple-400 rounded-none border-b border-purple-500">
+          <label for="name" class="w-16 text-right font-semibold mr-16">Email:</label>
+          <input type="text" id="email" name="email" class="flex-1 p-4 pl-0 bg-transparent outline-none text-white ml-16" onChange={inputHandler} />
         </div>
-        <div>
-          <label>Department:</label>
-          <select name="department" onChange={inputHandler}>
+        <div class="flex items-center bg-purple-400 rounded-none border-b border-purple-500">
+          <label for="department" class="w-16 text-right font-semibold ml-4 mr-16">Department:</label>
+          <select class="form-select mt-1 block w-full h-12  bg-purple-400 rounded-none border-purple-500 ml-12 text-white"onChange={inputHandler}>
             <option value="">---Select Department---</option>
             <option value="Marketing">Marketing</option>
             <option value="Advertising">Advertising</option>
             <option value="IT">IT</option>
           </select>
         </div>
-        <div>
-          <label>Company ID:</label>
-          <input type="text" name="companyId" onChange={inputHandler} />
+        <div class="flex items-center bg-purple-400 rounded-none border-b border-purple-500">
+          <label for="company" class="w-16 text-right font-semibold ml-4 mr-16">Company ID:</label>
+          <input type="text" id="company" name="company" class="flex-1 p-4 pl-0 bg-transparent outline-none text-white ml-12" onChange={inputHandler} />
         </div>
-        <div>
-          <label>Image:</label>
-          <input
-            type="file"
+        <div class="flex items-center bg-purple-400 rounded-b-lg mb-10">
+          <label for="name" class="w-20 text-right font-semibold mr-16">Image:</label>
+          <input type="file" class="flex-1 p-4 pl-0 bg-blue ml-12"
             name="imageURL"
             accept=".jpg, .png, .jpeg"
             onChange={(e) => setImage(e.target.files[0])}
           />
         </div>
+        <div class="flex items-center bg-purple-400 rounded-none border-b-lg border-purple-500">
+              <label for="description" class="w-24 text-right font-semibold">
+                        Description
+              </label>
+              <textarea class="ml-20 resize-none border rounded-md w-full bg-purple-400 text-white border-purple-500" 
+                 name="description"
+                 cols="30"
+                 rows="9"
+                 onChange={inputHandler}>
+              </textarea>
+          </div>
         <div>
-          <label>Description: </label>
-          <textarea
-            name="description"
-            cols="30"
-            rows="10"
-            onChange={inputHandler}
-          ></textarea>
-        </div>
-
-        <div>
-          <button disabled={isSubmitting} type="submit">
+          <button class="block w-full rounded bg-purple-500 py-3 hover:bg-pink-300 focus:shadow-outline focus:outline-none text-white font-bold shadow" type="button" disabled={isSubmitting} type="submit">
             Submit Request
           </button>
         </div>
+      </div>
       </form>
     </div>
   );
--- pages/login.js
+++ pages/login.js
@@ -22,21 +22,26 @@ export default function Login() {
 
   // click niyo lang yung login tapos punta kayo localhost:3000/admin
   return (
-    <div>
-      <h1>Login</h1>
+  
+    <div class="bg-white py-32 px-10 min-h-screen">
+    <div class="flex flex-1 justify-center">
+    <div class="bg-purple-300 rounded-lg sm:border-6 border-black px-4 lg:px-24 py-20 lg:max-w-xl sm:max-w-md w-full text-center">
+      <h1 class="text-5xl font-normal leading-normal mt-0 mb-10 text-pink-800 text-center">Login</h1>
       <form onSubmit={submitHandler}>
-        <div>
-          <label>Username:</label>
-          <input type="text" name="username" onChange={inputHandler} />
+        <div class="relative w-full mb-3">
+          <label class="block uppercase text-purple-900 text-xl font-bold text-left mb-2">Username:</label>
+          <input class="border-0 px-3 py-3 text-purple-900 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full" onChange={inputHandler} />
+        </div>
+        <div class="relative w-full mb-3">
+          <label class="block uppercase text-purple-900 text-xl font-bold text-left mb-2">Password:</label>
+          <input class="border-0 px-3 py-3  text-purple-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full" onChange={inputHandler} />
         </div>
         <div>
-          <label>Password:</label>
-          <input type="password" name="password" onChange={inputHandler} />
-        </div>
-        <div>
-          <button type="submit">Login</button>
+          <button type="submit"class="mt-8 block w-full rounded bg-purple-500 py-2 hover:bg-pink-300 focus:shadow-outline focus:outline-none text-white font-bold shadow"><p class="tracking-wider leading-10">Login</p></button>
         </div>
       </form>
+    </div>
+    </div>
     </div>
   );
 }
